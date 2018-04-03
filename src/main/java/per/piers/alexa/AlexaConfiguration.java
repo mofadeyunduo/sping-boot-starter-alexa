d296ad1b454f144d68ad7340a4c5176828c04730
Piers <keqiang.chen>
2018-04-03 11:30:22 +0800

    spring boot starter alexa

---
+++ src/main/java/per/piers/alexa/AlexaConfiguration.java
@@ -0,0 +1,19 @@
+package per.piers.alexa;
+
+import com.amazon.speech.speechlet.servlet.SpeechletServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import per.piers.alexa.helloworld.HelloWorldSpeechlet;
+
+@Configuration
+public class AlexaConfiguration {
+
+    @Bean
+    public ServletRegistrationBean<SpeechletServlet> registration() {
+        SpeechletServlet speechletServlet = new SpeechletServlet();
+        speechletServlet.setSpeechlet(new HelloWorldSpeechlet());
+        return new ServletRegistrationBean<>(speechletServlet, "/alexa");
+    }
+
+}
